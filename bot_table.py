02b469248effcfe3b48c907536c9bbca6e9a53b6
dj-kostya <nosorev_ka@gkl-kemerovo.ru>
2020-09-01 20:38:48 +0700

    Add course

--- bot_table.py
+++ bot_table.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+import datetime
 from get_table import get_table
 
 bot = telebot.TeleBot('1231314161:AAFNQ-RB_AIaihSEdL8cKoZhoe3YvgzIviQ')
@@ -21,36 +22,38 @@ def start_message(message):
                               f'Твое расписание на сегодня: ', reply_markup=None)
         get_timetable(message)
     except KeyError:
-        button_19137 = types.KeyboardButton(text="19137")
-        button_19144 = types.KeyboardButton(text="19144")
-
-        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-        keyboard.add(button_19137, button_19144)
+        keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=False)
+        for group in ('19137', '19144', '20137', '20144'):
+            btn = types.KeyboardButton(text=group)
+            keyboard.add(btn)
         bot.send_message(message.from_user.id, text='Привет! Какая у тебя группа?', reply_markup=keyboard)
 
     # bot.register_next_step_handler(message, get_group)
 
-
 @bot.message_handler(regexp='^\\d{5}$')
 @bot.message_handler(content_types=['text'])
 def get_timetable(message):
-
     try:
-        cache[message.chat.id] = {"group": int(message.text), "username": message.chat.username}
+        cache[message.chat.id] = {
+                                "group": int(message.text), 
+                                "username": message.chat.username, 
+                                "course":  (datetime.datetime.now().year // 100 - int(message.text[:2])) + 1
+                                }
     except ValueError:
         try:
             user = cache[message.chat.id]
         except KeyError:
             return
     user = cache[message.chat.id]
+    timetable = get_table(user['group'], day_of_week=None, course=user['course'])
+    print(user, timetable)
+    timetable_prepared = [f'{item[0]}: {item[1]}\n' for item in timetable ]
 
-    timetable = [f'{item[0]}: {item[1]}\n' for item in get_table(user['group'], False)]
-
-    str_timetable = "\n".join(timetable)
+    str_timetable = "\n".join(timetable_prepared)
     bot.send_message(message.from_user.id,
-                     text=f'Я запомнил, что твоя группа {user["group"]}!\n'
+                     text=f'Я запомнил, что твоя группа {user["group"]} и ты с {user["course"]} курса!\n'
                           f'Вот твое расписание на сегодня:\n{str_timetable}',
                      reply_markup=None)
-
+    # TODO: add keyboard to reply markup with сегодня, завтра . Пн, вт, ср, чт, пт, сб
 
 bot.polling()
