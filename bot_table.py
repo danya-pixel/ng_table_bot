b8e7e7e88439cedf712a9d943163378b037775c8
dj-kostya <nosorev_ka@gkl-kemerovo.ru>
2020-09-11 11:24:31 +0700

    add some cache for restarting

--- bot_table.py
+++ bot_table.py
@@ -22,14 +22,16 @@ days_names_lower = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб']
 days_names_chet_lower = [f'{day}\n(ч)' for day in days_names_lower]
 days_names_ne_chet_lower = [f'{day}\n(нч)' for day in days_names_lower]
 
+user_day_cache = {}
+
 
 def get_user_cache(chat_id):
     user = cache_user[chat_id]
     return user
 
 
-def send_timetable(user, day_of_week, is_tomorrow, is_even: bool or None = None, **kwargs):
-    day = Day(day_of_week=day_of_week, is_tomorrow=is_tomorrow, is_even=is_even)
+def send_timetable(user, day: Day, **kwargs):
+    # day = Day(day_of_week=day_of_week, is_tomorrow=is_tomorrow, is_even=is_even)
     timetable = get_table(group=user['group'], course=user['course'], day=day)
     print(user, timetable)
     timetable_prepared = [f'{item[0]}: {item[1]}\n' for item in timetable]
@@ -85,36 +87,45 @@ def set_group(message):
         except KeyError:
             return
 
-    today = datetime.datetime.now().weekday()
-    days_names_without_current = ['Сегодня', 'Завтра', *[day for day_idx, day in enumerate(days_names) if
-                                                         day_idx not in (today, (today + 1) % 6)]]
     btns_title = [['Сегодня', 'Завтра'], days_names_chet, days_names_ne_chet, ['Поменять группу']]
 
-    print(days_names_without_current)
-    send_timetable(user=user, day_of_week=None, is_tomorrow=False, reply_markup=generate_keyboard(btns_title))
+    try:
+        day = user_day_cache[user.get("user_id")]
+        del user_day_cache[user.get("user_id")]
+    except KeyError:
+        day = Day(day_of_week=None, is_tomorrow=False)
+    send_timetable(user=user, day=day, reply_markup=generate_keyboard(btns_title))
 
 
 @bot.message_handler(content_types=['text'])
 def catch_day_of_week(message):
-    try:
-        user = cache_user[message.chat.id]
-        chosen_day = message.text.lower()
-    except KeyError as err:
-        start_message(message)
-        return
-
+    chosen_day = message.text.lower()
     if chosen_day == 'сегодня':
-        send_timetable(user=user, day_of_week=None, is_tomorrow=False)
+        day = Day(day_of_week=None, is_tomorrow=False)
     elif chosen_day == 'завтра':
-        send_timetable(user=user, day_of_week=None, is_tomorrow=True)
+        day = Day(day_of_week=None, is_tomorrow=True)
     elif chosen_day in days_names_chet_lower:
-        send_timetable(user=user, day_of_week=days_names_chet_lower.index(chosen_day), is_tomorrow=False, is_even=True)
+        day = Day(day_of_week=days_names_chet_lower.index(chosen_day), is_tomorrow=False, is_even=True)
     elif chosen_day in days_names_ne_chet_lower:
-        send_timetable(user=user, day_of_week=days_names_ne_chet_lower.index(chosen_day), is_tomorrow=False,
-                       is_even=False)
+        day = Day(day_of_week=days_names_ne_chet_lower.index(chosen_day), is_tomorrow=False, is_even=False)
     elif chosen_day == 'поменять группу':
         bot.send_message(message.from_user.id, text='Какая у тебя группа?',
                          reply_markup=generate_keyboard([['19137', '19144'], ['20137', '20144']]))
+        return
+    else:
+        bot.send_message(message.from_user.id, text='Я теб не понимаю =(\nПопробуй воспользоваться клавиатурой')
+        return
+
+    try:
+        user = cache_user[message.chat.id]
+    except KeyError as err:
+        user_day_cache[message.chat.id] = day
+        start_message(message)
+        return
+    try:
+        send_timetable(user=user, day=day)
+    except ValueError:
+        pass
 
 
 if __name__ == '__main__':
